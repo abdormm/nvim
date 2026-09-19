ac1100b2c70e2993f8100661fbf7eb48f1b38a37
abdormm <abdelrahman200052@gmail.com>
2026-09-20 01:45:23 +0300

    docs: add notes related to filetype detection

--- filetype.lua
+++ filetype.lua
@@ -1,6 +1,14 @@
+---Notes:
+---`vim.filetype.match` checks in the following order:
+---    1. full path (if buf has a name) 
+---    2. only the filename (see :t expand())
+---    3. patterns with non-negative priority
+---    4. extension
+---    5. patterns with negative priority
+---    6. contents
 vim.filetype.add {
     extension = {
         pom = "xml",
         b = "jbc",
-    }
+    },
 }
